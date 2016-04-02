97829d342e0db2652b5d17c9879a701968c49556
single <single@axsoftware.net>
2016-04-02 14:03:28 +0800

    add some useful log

--- src/com/bisys/core/dao/impl/SurveyDaoImpl.java
+++ src/com/bisys/core/dao/impl/SurveyDaoImpl.java
@@ -15,34 +15,46 @@ public class SurveyDaoImpl implements SurveyDao {
 	@Autowired
 	private GeneralDao generalDao;
 
-	@Override
 	/*
-	 * 直接调用insert方法，不会做唯一性验证
-	 * 
+	 * 表格：vip_base_info_table
+	 * 操作：INSERT
+	 * 成功条件：查找【vip_base_info_table】表格，不存在这样的记录：user_name和要插入数据相同。
 	 *
 	 * */
-	public boolean insertIntoVipBaseTable(String username,String password , String register_date , String register_ip , String register_source , 
+	@Override
+	public boolean insertIntoVipBaseTable(String user_name,String password , String register_date , String register_ip , String register_source , 
 			String cellphone_zone , String ip_zone , String role_name , String permission_name)
 			throws Exception {
 		// TODO Auto-generated method stub
 		String sql = "call insert_vip_base_table('" 
-				+username+"',"+password+","+register_date+",'"+register_ip+"','"
-				+register_source+"',"+cellphone_zone+","+ip_zone+",'"+role_name+"','"+permission_name+"','"
-				+"');";
+				+user_name+"','"+password+"','"+register_date+"','"+register_ip+"','"
+				+register_source+"','"+cellphone_zone+"','"+ip_zone+"','"+role_name+"','"+permission_name+"');";
 		logger.info(sql);
 		return generalDao.saveEntity(sql, new Object[]{});
 	}
 	
+	/*
+	 * 表格：vip_dynamic_info_table
+	 * 操作：REPLACE
+	 * 成功条件：查找【vip_base_info_table】表格，存在这样的记录：user_name和要插入数据相同。
+	 *
+	 * */
 	@Override
-	public boolean replaceIntoVipDynamicTable(String username,int login_num , int test_num , String friend_1 , String friend_2 , String friend_3 , String friend_4 , String friend_5) throws Exception
+	public boolean replaceIntoVipDynamicTable(String user_name,int login_num , int test_num , String friend_1 , String friend_2 , String friend_3 , String friend_4 , String friend_5) throws Exception
 	{
 		String sql = "call replace_vip_dynamic_table ('"
-				+username+"',"+login_num+","+test_num+",'"+friend_1+"','"
+				+user_name+"',"+login_num+","+test_num+",'"+friend_1+"','"
 				+friend_2+"','"+friend_3+"','"+friend_4+"','"+friend_5+"');";
 		logger.info(sql);
 		return generalDao.saveEntity(sql, new Object[]{});
 	}
 	
+	/*
+	 * 表格：survey_table
+	 * 操作：INSERT
+	 * 成功条件：查找【survey_table】表格，不存在这样的记录：survey_name和要插入数据相同。
+	 *
+	 * */
 	@Override
 	public boolean insertIntoSurveyTable(String survey_name,String survey_desc , String survey_text , String survey_anwser , String start_time , String end_time , int status) throws Exception
 	{
@@ -53,6 +65,13 @@ public class SurveyDaoImpl implements SurveyDao {
 		return generalDao.saveEntity(sql, new Object[]{});
 	}
 	
+	/*
+	 * 表格：survey_user_table
+	 * 操作：INSERT
+	 * 成功条件1：查找【vip_base_info_table】表格，存在这样的记录：user_name和要插入数据相同。
+	 * 成功条件2：查找【survey_table】表格，存在这样的记录：survey_name和要插入数据相同。
+	 * 成功条件3：查找【survey_user_table】表格，不存在这样的记录：user_name、survey_name都和要插入数据相同。
+	 * */
 	@Override
 	public boolean insertIntoSurveyUserTable(String user_name,String survey_name , int answer_time , String answer , int transfer_flag) throws Exception
 	{
