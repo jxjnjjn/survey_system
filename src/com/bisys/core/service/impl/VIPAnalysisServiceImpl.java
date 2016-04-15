f0bb16849f8936af3a6ba12015b578b93bd0eeb9
single <single@axsoftware.net>
2016-04-15 16:20:08 +0800

    update

--- src/com/bisys/core/service/impl/VIPAnalysisServiceImpl.java
+++ src/com/bisys/core/service/impl/VIPAnalysisServiceImpl.java
@@ -1,6 +1,5 @@
 package com.bisys.core.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -12,7 +11,6 @@ import com.bisys.core.dao.UserDao;
 import com.bisys.core.entity.survey.VipAnalysisEntity;
 import com.bisys.core.entity.survey.VipTrendAnalysisEntity;
 import com.bisys.core.entity.survey.VipZoneAnalysisEntity;
-import com.bisys.core.util.JsonPageInfo;
 
 @Service("VIPAnalysisServiceImpl")
 public class VIPAnalysisServiceImpl{
@@ -42,33 +40,5 @@ public class VIPAnalysisServiceImpl{
 		return surveyDao.getVipZoneAnalysis();
 	}
 	
-	
-	public List<VipAnalysisEntity> getEntityInfo(List<VipAnalysisEntity> result , int pageNo) throws Exception
-	{
-		List<VipAnalysisEntity> tableList = new ArrayList<VipAnalysisEntity>();
-		
-		if(null != result)
-		{
-			int srcSize = result.size();
-			int starlow = everyPageNum*(pageNo-1);
 
-			if(starlow < srcSize && starlow >= 0)
-			{
-				int endlow = ((starlow + everyPageNum)>srcSize)?srcSize:(starlow + everyPageNum);
-						
-				for(int i=starlow;i<endlow;i++)
-				{
-					tableList.add(result.get(i));
-				}
-			}
-		}
-
-		return tableList;
-	}
-	
-	public JsonPageInfo getPageInfo(int length , int pageNo) throws Exception
-	{
-		JsonPageInfo result = new JsonPageInfo(pageNo , everyPageNum , length);
-		return result;
-	}
 }
