f152a178228c046a29fec15d8348bc967a5796f0
single <single@axsoftware.net>
2016-04-24 16:06:42 +0800

    update

--- src/com/bisys/core/dao/SurveyDao.java
+++ src/com/bisys/core/dao/SurveyDao.java
@@ -22,7 +22,8 @@ public interface SurveyDao {
 	public List<VipAnalysisEntity> getVipAnalysis(String today) throws Exception;
 	public List<VipTrendAnalysisEntity> getVipTrendAnalysis(String start, String end) throws Exception;
 	public List<VipZoneAnalysisEntity> getVipZoneAnalysis() throws Exception;
-	public boolean insertIntoSurveyUserTable(String user_name,String survey_name , int answer_time , String answer , int transfer_flag) throws Exception; 
+	boolean insertIntoSurveyUserTable(String user_name, String survey_name, int answer_time, String answer,
+			String answer_fillinblank, int transfer_flag) throws Exception;
 	public List<VipUserSurveyInfoEntity> getVipSurveyInfo(String user_name) throws Exception;
 	public List<VipSurveyFriendInfoEntity> getVipFriendSurveyInfo(String user_name) throws Exception; 
 	public List<VipListEntity> getVipList() throws Exception; 
